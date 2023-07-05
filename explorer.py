6893c0f5c8b04e497a056f369f55554a572532fd
Richard Decal <richard.decal@ncf.edu>
2023-07-05 10:59:06 -0400

    added support for stepping on minibatch

--- explorer.py
+++ explorer.py
@@ -10,9 +10,12 @@ import matplotlib.pyplot as plt
 
 # list all schedulers here
 st.sidebar.markdown("# Learning rate scheduler")
+
+
 @st.cache_data
 def get_timm_schedulers():
-    """Get list of timm schedulers
+    """
+    Get list of timm schedulers
 
     Note: we cannot use the class names from timm.scheduler because they are not the same as the
     names used in timm.scheduler.scheduler_factory.create_scheduler_v2. Thus, we need to parse
@@ -27,15 +30,17 @@ def get_timm_schedulers():
             # extract the sceduler name using regex
             pattern = r"[a-zA-Z]+"
             sched_name = re.findall(pattern, sched_name)[0]
-            schedulers.append( sched_name)
-    assert len(schedulers)>3, "Error parsing scheduler names"
+            schedulers.append(sched_name)
+    assert len(schedulers) > 3, "Error parsing scheduler names"
     return schedulers
+
 
 name = st.sidebar.selectbox(
     "Scheduler",
     options=list(get_timm_schedulers()),
 )
 lr = st.sidebar.number_input("Learning rate", value=1.0)
+
 
 @st.cache_data
 def get_scheduler_kwargs():
@@ -47,8 +52,9 @@ def get_scheduler_kwargs():
         for arg_name, arg in scheduler_args.items()
         if arg_name not in {"optimizer", "sched"}
     }
-    assert len(scheduler_args)>0, "Error parsing scheduler args"
+    assert len(scheduler_args) > 0, "Error parsing scheduler args"
     return scheduler_args
+
 
 default_args = get_scheduler_kwargs()
 
@@ -58,6 +64,18 @@ for arg_name, default_value in default_args.items():
     if isinstance(default_value, bool):
         value = st.sidebar.checkbox(arg_name, value=default_value)
     elif isinstance(default_value, (int, float)):
+        # exception: set default of updates_per_epoch to 100 instead of the default 0
+        if arg_name == "updates_per_epoch":
+            st.sidebar.markdown(
+                f"Note: timm default for {arg_name}={default_value}; overriding to 10 here"
+            )
+            default_value = 10
+        # exception: set default of num_epochs to 100 instead of the default 300
+        if arg_name == "num_epochs":
+            st.sidebar.markdown(
+                f"Note: timm default for {arg_name}={default_value}; overriding to 50 here"
+            )
+            default_value = 50
         value = st.sidebar.number_input(arg_name, value=default_value)
     elif isinstance(default_value, str):
         value = st.sidebar.text_input(arg_name, value=default_value)
@@ -76,45 +94,84 @@ for arg_name, default_value in default_args.items():
         if value == "None":
             value = None
     else:
-        raise NotImplementedError(f"Unknown type for {arg_name} {type(default_value)}: {default_value}")
+        raise NotImplementedError(
+            f"Unknown type for {arg_name} {type(default_value)}: {default_value}"
+        )
 
     override_args[arg_name] = value
+
 
 # create a scheduler with the user's args
 dummy_model = timm.create_model("resnet18")
 dummy_optimizer = create_optimizer_v2(dummy_model, opt="sgd", lr=lr)
 
 
-scheduler, _ = create_scheduler_v2(optimizer=dummy_optimizer, sched=name, **override_args)
-assert isinstance(scheduler, Scheduler), f"Expected scheduler to be a timm.scheduler.Scheduler, got {type(scheduler)}"
+scheduler, _ = create_scheduler_v2(
+    optimizer=dummy_optimizer, sched=name, **override_args
+)
+assert isinstance(
+    scheduler, Scheduler
+), f"Expected scheduler to be a timm.scheduler.Scheduler, got {type(scheduler)}"
+
 
 # simulate a training loop with the scheduler using num_epochs value and plot the LR
+
+
+def get_current_lr(optimizer):
+    """Get the current LR from the optimizer"""
+    lrl = [param_group["lr"] for param_group in optimizer.param_groups]
+    lr = sum(lrl) / len(lrl)
+    return lr
+
+
 num_epochs = override_args["num_epochs"]
+epoch_ticks = []
 lrs = []
 lrs2 = []
 if override_args["cycle_mul"] > 1:
     num_epochs = int(num_epochs * override_args["cycle_mul"])
 for epoch in range(num_epochs):
-    scheduler.step(epoch)
-    # get the LR for this epoch from the optimizer
-    lr = dummy_optimizer.param_groups[0]["lr"]
-    #assert isinstance(lr, float), f"Expected LR to be a float, got {type(lr)}"
-    lrs.append(lr)
-    lrs2.append(scheduler._get_lr(epoch))
-    # lrs2.append(scheduler.get_epoch_values(epoch))
-assert len(lrs) == num_epochs, f"Expected {num_epochs} LR values, got {len(lrs)}"
+    # note: need to step at the beginning of epoch, since we use 0-indexing for epoch
+    scheduler.step(epoch=epoch)  # will have no effect if step_on_epochs is False
+
+    epoch_ticks.append(epoch)
+    lrs.append(get_current_lr(dummy_optimizer))
+
+    # use 1-indexing for batch_i to get the expected num batches
+    for batch_i in range(1, override_args["updates_per_epoch"]):
+        global_batch = epoch * override_args["updates_per_epoch"] + batch_i
+        global_epoch = global_batch / override_args["updates_per_epoch"]
+        # will have no effect if step_on_epochs is True
+        scheduler.step_update(num_updates=global_batch)
+        epoch_ticks.append(global_epoch)
+        lrs.append(get_current_lr(dummy_optimizer))
+
+    # lrs2.append(
+    # scheduler._get_lr(epoch)
+    # )  # sanity check: get_lr should be the same as optimizer lr
+
+assert len(lrs) == num_epochs * override_args["updates_per_epoch"], (
+    f"Expected {num_epochs * override_args['updates_per_epoch']} LR values, "
+    f"got {len(lrs)}"
+)
 
 fig = plt.figure()
-plt.plot(lrs)
+plt.scatter(epoch_ticks, lrs, color="red", s=1, marker=".")
 plt.title(f"Learning rate schedule for {name}")
 plt.xlabel("Epoch")
 plt.ylabel("Learning rate")
+# include all the override args as a caption
+# caption = "\n".join(
+#     f"{arg_name}: {arg_value}" for arg_name, arg_value in override_args.items()
+# )
+plt.ylim(bottom=0)
 st.pyplot(fig)
 
+print(lrs)
 
-fig = plt.figure()
-plt.plot(lrs2)
-plt.title(f"Learning rate schedule for {name}")
-plt.xlabel("Epoch")
-plt.ylabel("Learning rate")
-st.pyplot(fig)
+# fig = plt.figure()
+# plt.plot(lrs2)
+# plt.title(f"Learning rate schedule for {name}")
+# plt.xlabel("Epoch")
+# plt.ylabel("Learning rate")
+# st.pyplot(fig)
